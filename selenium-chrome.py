2ec6957862e4ec6d799aa8773259813e13989cac
a67558491 <112803866+a67558491@users.noreply.github.com>
2022-09-04 18:09:26 +0800

    Update selenium-chrome.py

--- selenium-chrome.py
+++ selenium-chrome.py
@@ -416,7 +416,7 @@ def main():
     global driver, processExceptCount
     options = Options()
     options.add_experimental_option("excludeSwitches", ["enable-logging"])
-    options.add_argument("headless")
+    options.add_argument("headless") #options.add_argument("headless")#options.add_argument("disable-gpu")
     options.add_argument("disable-gpu")
     driver = webdriver.Chrome(
         service=Service(
